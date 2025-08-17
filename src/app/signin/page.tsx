9cc5a18b301cdb4179ef7fffd09bbfa2de06ba84
vaman971 <vaman971@gmail.com>
2025-08-18 00:42:52 +0530

    sign in page modified

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,12 @@
 // /app/signin/page.tsx
+"use client";
+
+import React, { Suspense } from "react";
 import SignInPage from "@/sections/SignInPage";
 
 export default function Page() {
-  return <SignInPage />;
-}
+  return(
+  <Suspense fallback={<div>Loading...</div>}>
+      <SignInPage />
+  </Suspense>
+)}
